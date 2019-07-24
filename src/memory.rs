f4ffc98709be1d42ac3e052bd583a5fad80e9d4a
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2019-07-24 22:22:09 +0200

    Add license headers

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,23 @@
+/*
+    prefault
+    Copyright (C) 2019 the prefault developers
+
+    This file is part of prefault.
+
+    Prefault is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Prefault is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Prefault.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 use libc;
 use std::ffi;
 use std::fs::File;
